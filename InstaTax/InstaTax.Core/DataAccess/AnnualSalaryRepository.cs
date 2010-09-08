d5905d77727945a757ee6a6a38d740e1986829db
Siva <vsiva@thoughtworks.com>
2010-09-08 16:50:00 +0530

    NHibernate commit

---
+++ InstaTax/InstaTax.Core/DataAccess/AnnualSalaryRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using NHibernate;
+
+namespace InstaTax.Core
+{
+    public class AnnualSalaryRepository : NHibernateSetup, IAnnualSalaryRepository
+    {
+        public void SaveAnnualSalary(AnnualSalary salary)
+        {
+            Session.Save(salary);
+        }
+
+        public AnnualSalary GetAnnualSalary(User user)
+        {
+            IQuery query = Session.CreateQuery("from AnnualSalary where userId = '" + user.Id + "'");
+            IList<AnnualSalary> liSalary = query.List<AnnualSalary>();
+            if (liSalary.Count > 0)
+                return liSalary[0];
+
+            return null;
+        }
+    }
+}
---
+++ InstaTax/InstaTax.Core/DataAccess/IAnnualSalaryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InstaTax.Core
+{
+    public interface IAnnualSalaryRepository
+    {
+        void SaveAnnualSalary(AnnualSalary salary);
+        AnnualSalary GetAnnualSalary(User user);
+    }
+}
