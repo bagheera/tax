3e205227b710495638b102040bca36b302659d1c
unknown <rangank@.(none)>
2010-10-05 17:41:14 +0530

    Added functionality for UserRepository to persist user information

---
+++ InstaTax/InstaTax.Tests/TestUserRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using InstaTax.Core;
+using InstaTax.Core.DomainObjects;
+using NHibernate;
+using NUnit.Framework;
+
+namespace InstaTax.Tests
+{
+    [TestFixture]
+    public class TestUserRepository 
+    {
+        [Test]
+        public void ShouldSaveAndLoadUser()
+        {
+            IUserRepository repository = new UserRepository();
+
+            var password = new Password { PasswordString = "abc" };
+            var email = new EmailAddress("b@c.com");
+            var user = new User(email, password);
+
+            user.FromMetro = true;
+            user.HousingLoanInterestAmount = 8100.0;
+            user.RentPaid = 8000.00;
+            
+            user.Repository = repository;
+            
+            user.Register();
+            
+            User actualUser = repository.LoadByEmailId(email);
+
+            Assert.IsNotNull(actualUser);
+            Assert.AreEqual(email, actualUser.EmailAddress);
+            Assert.AreEqual(user.FromMetro, actualUser.FromMetro);
+            Assert.AreEqual(user.HousingLoanInterestAmount, actualUser.HousingLoanInterestAmount);
+            Assert.AreEqual(user.RentPaid, actualUser.RentPaid);
+        }
+    }
+}
