72e1373a342b1c9173d6199bddb9db5e6fe11c9d
ashish mishra <ashishkm@thoughtworks.com>
2010-10-05 17:55:07 +0530

    functional test case for housing loan interest

---
+++ InstaTax/InstaTax.Tests/Functional/AnnualSalaryFunctionalTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InstaTax.Core;
+using InstaTax.Core.DomainObjects;
+using NUnit.Framework;
+
+namespace InstaTax.Tests.Functional
+{
+    [TestFixture]
+    public class AnnualSalaryFunctionalTest
+    {
+        [Test]
+        public void MustCalculateNetPayableTaxWithAnnualSalaryOtherIncomesAndChapter6Investments()
+        {
+            var taxPayer = new User(50000, true, Gender.Female);
+            taxPayer.HousingLoanInterest = new HousingLoanInterest(10000);
+
+            TaxSlabs ts = TaxSlabs.GetInstance();
+            AnnualSalary asal = new AnnualSalary
+            {
+                Basic = 810000.50,
+                Hra = 1000,
+                ProfessionalTax = 100,
+                SpecialAllowance = 10
+            };
+
+            OtherIncomes otherIncomes = new OtherIncomes();
+            otherIncomes.Add(new OtherIncomeItem("Income from Interest", 4000.0));
+            otherIncomes.Add(new OtherIncomeItem("Income from House Rent", 8000.0));
+
+            Chapter6Investments investments = new Chapter6Investments();
+            investments.Add(new LifeInsurance(50000));
+            investments.Add(new Elss(60000));
+
+            TaxStatement stmt = new TaxStatement(asal);
+            stmt.OtherIncomes = otherIncomes;
+            stmt.Chapter6Investments = investments;
+
+            Assert.AreEqual(120473,
+                stmt.CalculateNetPayableTax(taxPayer), 2);
+        }
+    }
+}
