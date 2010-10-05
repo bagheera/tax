2dd2b77127f20831f148b133bfd4ce969168f955
unknown <rangank@.(none)>
2010-10-05 18:27:56 +0530

    Moved OtherIncomes.cs and OtherIncomeItem.cs into the DomainObjects namespace

---
+++ InstaTax/InstaTax.Core/DomainObjects/OtherIncomeItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace InstaTax.Core.DomainObjects
+{
+    public class OtherIncomeItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Amount { get; set; }
+
+        public OtherIncomeItem(string name, double amount)
+        {
+            if (name == null) throw new ArgumentNullException("name","Name Cannot be Null");
+            if (name.Length == 0) throw new ArgumentException("Name must be specified", "name");
+            if (amount <= 0.0) throw new ArgumentException("Amount cannot be zero or negative", "amount");
+            Name = name;
+            Amount = amount;
+        }
+    }
+}
---
+++ InstaTax/InstaTax.Core/DomainObjects/OtherIncomes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InstaTax.Core.DomainObjects
+{
+    public class OtherIncomes
+    {
+        private readonly List<OtherIncomeItem> otherIncomeItems = new List<OtherIncomeItem>();
+
+        public void Add(OtherIncomeItem otherIncomeItemToAdd)
+        {
+            if (otherIncomeItemToAdd == null) throw new ArgumentNullException();
+            otherIncomeItems.Add(otherIncomeItemToAdd);
+        }
+
+        public int Count
+        {
+            get { return otherIncomeItems.Count; }
+        }
+
+        public double CalculateTotalAmount()
+        {
+            return otherIncomeItems.Sum(a => a.Amount);
+        }
+    }
+}
